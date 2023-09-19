580cd1f936614c75f468c7435191045ed179ae51
Jx64 <jesusdv09@gmail.com>
2023-09-19 17:11:33 -0500

    First commit

---
+++ src/main/java/com/example/eliminatorias/EliminatoriasApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.eliminatorias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EliminatoriasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EliminatoriasApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/eliminatorias/EliminatoriasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eliminatorias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EliminatoriasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
