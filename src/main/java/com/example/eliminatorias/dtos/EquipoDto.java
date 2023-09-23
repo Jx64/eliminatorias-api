d141bd8cc625fe05cccab9e89541d3b3752ec221
Jx64 <jesusdv09@gmail.com>
2023-09-23 00:57:19 -0500

    Added dto validation

--- src/main/java/com/example/eliminatorias/dtos/EquipoDto.java
+++ src/main/java/com/example/eliminatorias/dtos/EquipoDto.java
@@ -1,12 +1,19 @@
 package com.example.eliminatorias.dtos;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class EquipoDto{
 
     private Long id;
+
+    @NotBlank
     private String nombre;
+
+    @NotBlank
     private String bandera;
+
+    @NotBlank
     private String directorTecnico;
 }
--- src/main/java/com/example/eliminatorias/dtos/PartidoDto.java
+++ src/main/java/com/example/eliminatorias/dtos/PartidoDto.java
@@ -1,6 +1,9 @@
 package com.example.eliminatorias.dtos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
@@ -8,9 +11,18 @@ import java.time.LocalDate;
 public class PartidoDto{
 
     private Long id;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate fecha;
+
+    @NotBlank
     private String estadio;
+
+    @NotBlank
     private String arbitro;
+
     private EquipoDto equipoLocal;
+
     private EquipoDto equipoVisitante;
 }
